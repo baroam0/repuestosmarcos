526ed30c266973868c0fb8980cc74618c03eb16f
Abel Barrios <barriosabel0@hotmail.com>
2021-10-29 16:56:05 -0300

    Se agrega parte de mercaderias

---
+++ mercaderias/models.py
@@ -0,0 +1,36 @@
+
+from django.db import models
+
+
+class Unidad(models.Model):
+    """Clase para las Unidades """    
+    descripcion = models.CharField(
+        max_length=255,blank=False, null=False, unique=True)
+
+    def __str__(self):
+        """Retorno del nombre de la Unidad"""
+        return self.descripcion
+
+    class Meta:
+        """Meta de la Unidad """
+        verbose_name_plural = "Unidades"
+
+
+class Mercaderia(models.Model):
+    """Clase para las Mercaderias """
+    descripcion = models.CharField(
+        max_length=255,blank=False, null=False, unique=True)
+    unidad = models.ForeignKey(Unidad, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        """Retorno del nombre de la Mercaderia"""
+        return self.descripcion
+
+    class Meta:
+        """Meta de la Mercaderia """
+        verbose_name_plural = "Mercaderias"
+
+
+
+# Create your models here.
