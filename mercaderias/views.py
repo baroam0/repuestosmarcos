a1e7a7000bc35fe47a8b5fbad9c9a5f55a6fb07c
Abel Barrios <barriosabel0@hotmail.com>
2021-11-01 09:30:34 -0300

    Commit para traer cambios

--- mercaderias/views.py
+++ mercaderias/views.py
@@ -1,3 +1,55 @@
+
 from django.shortcuts import render
+
+from .models import Mercaderia, Unidad
+
+
+def listadomercaderia(request):
+    resultados = None
+    if "txtBuscar" in request.GET:
+        parametro = request.GET.get("txtBuscar")
+        resultados = Mercaderia.objects.filter(descripcion__icontains=parametro)
+
+    return render(
+        request,
+        "mercaderias/listadomercaderias.html",
+        {
+            "resultados": resultados
+        }
+    )
+
+
+def nuevomaterial(request):
+    if request.POST:
+        form = MaterialForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "SE HA GRABADO EL MATERIAL")
+            return redirect('/listadomaterial')
+        else:
+            return render(request, 'materiales/material_edit.html', {"form": form})
+    else:
+        form = MaterialForm()
+        return render(request, 'materiales/material_edit.html', {"form": form})
+
+
+def editarmaterial(request, pk):
+    consulta = Material.objects.get(pk=pk)
+    if request.POST:
+        form = MaterialForm(request.POST, instance=consulta)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "SE HA ACTUALIZADO EL MATERIAL")
+            return redirect('/listadomaterial')
+        else:
+            return render(request, 'materiales/material_edit.html', {"form": form})
+    else:
+        form = MaterialForm(instance=consulta)
+        return render(request,
+            'materiales/material_edit.html',
+            {"form": form}
+        )
+
+    
 
 # Create your views here.
