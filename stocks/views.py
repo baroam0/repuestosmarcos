7b142039ac664261f0c1838b9f3049e6e8c53561
Abel Barrios <barriosabel0@hotmail.com>
2022-06-10 14:34:29 -0300

    Commit para traer cambios

--- stocks/views.py
+++ stocks/views.py
@@ -1,12 +1,21 @@
 
+
+from django.core.paginator import Paginator
 from django.db.models import F
 from django.shortcuts import render
 from mercaderias.models import Mercaderia
 
 
 def stockminimos(request):
-    
     resultados = Mercaderia.objects.filter(cantidad__lte=F('minimo'))
+    paginador = Paginator(resultados, 10)
+
+    if "page" in request.GET:
+        page = request.GET.get('page')
+    else:
+        page = 1
+
+    resultados = paginador.get_page(page)
 
     return render(
         request,
